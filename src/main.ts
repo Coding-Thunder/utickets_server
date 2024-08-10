919b4c6980f2c20f1783bc9eb3b1abf99673da32
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-10 14:26:45 +0530

    ADD: new build release

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   // Enable CORS with detailed configuration
-  app.enableCors({
-    origin: 'https://universalticketss.com/', // Replace with your Next.js frontend origin
-    // origin: 'http://localhost:3000', // Replace with your Next.js frontend origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers
-    credentials: true, // Allow credentials (e.g., cookies)
-    preflightContinue: false, // Let Nest.js handle preflight OPTIONS requests
-  });
+  app.enableCors();
   await app.listen(process.env.DATABASE_PORT);
 }
 bootstrap();
