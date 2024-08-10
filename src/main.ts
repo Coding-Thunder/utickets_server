8e49a068f0d3cf9d47f31e8045349a1556d89450
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-10 15:28:41 +0530

    ADD: new build release

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: "https://www.universalticketss.com", // Ensure no trailing slash
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization", // Add other headers if needed
+    credentials: true, // Enable credentials
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+
   await app.listen(process.env.DATABASE_PORT);
 }
 bootstrap();
