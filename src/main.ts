c656c46787a6bac0ad8b9f6c18738d721479bc84
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-10 16:09:22 +0530

    ADD: new build release

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,24 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: "https://universalticketss.com", // Ensure no trailing slash
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type,Authorization", // Add other headers if needed
-    credentials: false, // Enable credentials
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://universalticketss.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+      return res.status(200).json({});
+    }
+    next();
   });
+  // app.enableCors({
+  //   origin: "https://universalticketss.com", // Ensure no trailing slash
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   allowedHeaders: "Content-Type,Authorization", // Add other headers if needed
+  //   credentials: false, // Enable credentials
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
 
 
   await app.listen(process.env.DATABASE_PORT);
