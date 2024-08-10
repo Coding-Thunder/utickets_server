560eeeaf35a6124e9c0f8511d177899d115a4590
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-08-10 14:34:21 +0530

    ADD: new build release

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,9 @@ async function bootstrap() {
 
   // Enable CORS with detailed configuration
   app.enableCors({
-    origin: 'https://universalticketss.com', // Allow your Next.js production URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // If you're using cookies or other credentials
+    allowedHeaders: '*',
+    origin: '*',
   });
-
   await app.listen(process.env.DATABASE_PORT);
 }
 bootstrap();
