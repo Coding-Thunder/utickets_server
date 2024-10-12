298ccbb961e3f5429f131dff0b276cb05e634a7a
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2024-10-12 12:46:36 +0530

    ADD: API Keys for production

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,29 @@ import { AppModule } from './modules/app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Use global validation pipe
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
-  // CORS configuration
+  app.use((req: { method: string; }, res: { header: (arg0: string, arg1: string) => void; status: (arg0: number) => { (): any; new(): any; json: { (arg0: {}): any; new(): any; }; }; }, next: () => void) => {
+    res.header("Access-Control-Allow-Origin",
+      "https://universalticketss.com"
+      // "http://localhost:3001"
+    );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+      return res.status(200).json({});
+    }
+    next();
+  });
   app.enableCors({
     origin: "https://universalticketss.com", // Ensure no trailing slash
+    // origin: "http://localhost:3001", // Ensure no trailing slash
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    // allowedHeaders: "*", // Add other headers if needed
   });
 
-  // Start the application
-  await app.listen(process.env.PORT || 3000); // Default to port 3000 if not specified
+
+  await app.listen(process.env.DATABASE_PORT || 3002);
 }
 bootstrap();
